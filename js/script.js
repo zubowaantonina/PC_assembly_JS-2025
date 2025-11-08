cbacb02148a1dcb22e4b728041a47d6a2c2eba74
zubowaantonina <zubowaantonina@yandex.ru>
2025-11-08 23:05:28 +0300

    Урок 1. Аккордеон

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// const contents = document.querySelectorAll('.program-line__content')
+const title = document.querySelectorAll('.program-line__title')
+const descr = document.querySelectorAll('.program-line__descr')
+
+// contents.forEach((elem) => {
+//     const title = elem.querySelector('.program-line__title')
+//     const descr = elem.querySelector('.program-line__descr')
+
+//     title.addEventListener('click', () => {
+
+//         descr.classList.toggle('active')
+//     })
+// })
+
+title.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        descr.forEach((item) => {
+            item.classList.remove('active')
+        })
+        descr[index].classList.add('active')
+    })
+
+})
+
