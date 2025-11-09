25b5661774f6f461727a20c935790c2f96166fe1
zubowaantonina <zubowaantonina@yandex.ru>
2025-11-09 21:23:31 +0300

    закрытие модального окна по клику на крестик

--- js/modal.js
+++ js/modal.js
@@ -1,16 +1,37 @@
 const modalBtn = document.querySelector('.modal__button')
 const modal = document.querySelector('.modal')
+const modalHeader = document.querySelector('.modal__header')
+const courseBtn = document.querySelector('.course__button')
 
+const modalClose = () => {
+    modal.style.display = ''
+}
 
-modalBtn.addEventListener('click', () => {
+const modalOpen = () => {
     modal.style.display = 'flex'
-
+}
+modalBtn.addEventListener('click', () => {
+    modalOpen()
+})
+courseBtn.addEventListener('click', () => {
+    modalOpen()
 })
 modal.addEventListener('click', () => {
     const modalContent = event.target.closest('.modal__inner')
-
     if (!modalContent) {
-        modal.style.display = ''
+        modalClose()
     }
 })
 
+modalHeader.insertAdjacentHTML('beforeBegin', `
+   <div class="closeModal">
+   <img src="img/icon_cross.svg" alt="close">
+   </div>
+    `
+)
+const closeBtn = document.querySelector('.closeModal')
+
+closeBtn.addEventListener('click', (event) => {
+    event.preventDefault();
+    modalClose()
+})
