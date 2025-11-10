7f0c3f407e8c5022a3e51b6eea3d137754b91afc
zubowaantonina <zubowaantonina@yandex.ru>
2025-11-10 22:47:48 +0300

    Урок 3. Таймер

---
+++ js/timer.js
@@ -0,0 +1,72 @@
+
+const daysBlock = document.querySelector('.timer__days')
+const hoursBlock = document.querySelector('.timer__hours')
+const minutesBlock = document.querySelector('.timer__minutes')
+const secondsBlock = document.querySelector('.timer__seconds')
+
+let intervalId;
+const padZero = (elem) => {
+    if (elem < 10) {
+        return (elem = `0${elem}`);
+    } else {
+        return elem;
+    }
+};
+const numWord = (value, words) => {
+    value = Math.abs(value) % 100;
+    const lastNum = value % 10;
+    if (value > 10 & value < 20) return words[2];
+    if (lastNum > 1 && lastNum < 5) return words[1];
+    if (lastNum === 1) return words[0]
+    return words[2]
+}
+const redNum = () => {
+    daysBlock.style.color = 'red';
+    hoursBlock.style.color = 'red';
+    minutesBlock.style.color = 'red';
+    secondsBlock.style.color = 'red';
+}
+const updateTimer = () => {
+    const date = new Date()
+    const dateDeadline = new Date('13 november 2025').getTime()
+    const timeRemaining = (dateDeadline - date) / 1000
+
+    const days = Math.floor(timeRemaining / 3600 / 24);
+    const hours = Math.floor((timeRemaining / 3600) % 24);
+    const minutes = Math.floor((timeRemaining / 60) % 60);;
+    const seconds = Math.floor(timeRemaining % 60)
+
+    const fDays = padZero(days)
+    const fHours = padZero(hours)
+    const fMinutes = padZero(minutes)
+    const fSeconds = padZero(seconds)
+
+    daysBlock.textContent = fDays
+    hoursBlock.textContent = fHours
+    minutesBlock.textContent = fMinutes
+    secondsBlock.textContent = fSeconds
+
+    if (timeRemaining <= 0) {
+        daysBlock.textContent = "00";
+        hoursBlock.textContent = "00";
+        minutesBlock.textContent = "00";
+        secondsBlock.textContent = "00";
+        redNum()
+        clearInterval(intervalId);
+    }
+    daysBlock.nextElementSibling.textContent = numWord(days, ['день', 'дня', 'дней'])
+    hoursBlock.nextElementSibling.textContent = numWord(hours, ['час', 'часа', 'часов'])
+    minutesBlock.nextElementSibling.textContent = numWord(minutes, ['минута', 'минуты', 'минут'])
+    secondsBlock.nextElementSibling.textContent = numWord(seconds, ['секунда', 'секунды', 'секунд'])
+
+
+
+}
+
+updateTimer()
+intervalId = setInterval(updateTimer, 500);
+
+
+
+
+// console.log(numWord(0, ['час', 'часа', 'часов']));
