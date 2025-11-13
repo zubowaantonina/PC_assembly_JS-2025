866317f3ab20c58cd7b39d3938946d6df26bea97
zubowaantonina <zubowaantonina@yandex.ru>
2025-11-13 22:01:03 +0300

    рандомное число

--- js/progressBar.js
+++ js/progressBar.js
@@ -6,15 +6,20 @@ const progressBar = () => {
 
     myBar.style.width = percentage + "%";
     number.textContent = sum;
-
-
   }
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // Максимум и минимум включаются
+}
+newSum=Math.round(getRandomIntInclusive(350000, 600000)/10000);
+// console.log(newSum);
 
   let sum = 0;
   let progress = 0;
   const interval = setInterval(() => {
     
-    if (progress >= 40) {
+    if (progress >= newSum) {
       clearInterval(interval);
     } else {
       progress += 1;
